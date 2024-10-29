4c8501ddb9cec91c6e623fc759c5bd0e379888e7
prodiGamer1000 <168657105+prodiGamer1000@users.noreply.github.com>
2024-10-29 20:14:42 -0300

    sadcaqsda

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,23 @@
 const snake_head = document.querySelector('.snake_head')
 const snake_body = document.querySelector('.snake_body')
 
-let snake_head_pos = snake_head.classList[1]
+let snake_head_pos
 let snake_wheight = 3
 
 document.addEventListener('keydown', (e)=>{
+    snake_head_pos = snake_head.classList[1]
     if(e.keyCode == 39){
-        console.log(snake_head_pos)
+        direita(snake_head_pos, a)
     }else if(e.keyCode == 37){
-        console.log(snake_head_pos)
+        esquerda(snake_head_pos, a)
     }
 })
 
 
 function cima(x, y){}
 function baixo(x, y){}
-function esquerda(x, y){}
+function esquerda(x, y){
+    document.querySelector('.'+x)
+    snake_head.classList.remove[1]
+}
 function direita(x,y){}
